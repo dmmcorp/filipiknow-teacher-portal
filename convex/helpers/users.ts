bdd1fd5c2a6914aa435620ccc9df825b6661628d
ReformaMark <mark.reforma0117@gmail.com>
2025-08-10 19:36:36 +0800

    fix: types of section from string to Id<"sections>

--- convex/helpers/users.ts
+++ convex/helpers/users.ts
@@ -1,7 +1,8 @@
-import { createAccount } from "@convex-dev/auth/server";
-import { ConvexError } from "convex/values";
-import { internal } from "../_generated/api";
-import { ActionCtx } from "../_generated/server";
+import { createAccount } from '@convex-dev/auth/server';
+import { ConvexError } from 'convex/values';
+import { internal } from '../_generated/api';
+import { Id } from '../_generated/dataModel';
+import { ActionCtx } from '../_generated/server';
 
 export async function createUser(
   ctx: ActionCtx,
@@ -17,10 +18,10 @@ export async function createUser(
 ) {
   const { fname, lname, email, password, gradeLevel, section } = userData;
 
-  const role = "student";
-
+  const role = 'student';
+  const sectionId = section as Id<'sections'>;
   const create = await createAccount(ctx, {
-    provider: "password",
+    provider: 'password',
     account: {
       id: email,
       secret: password,
@@ -37,11 +38,11 @@ export async function createUser(
   if (create.user._id) {
     const studentId = await ctx.runMutation(internal.students.createStudent, {
       userId: create.user._id,
-      section,
+      section: sectionId,
       gradeLevel,
     });
     if (!studentId) {
-      throw new ConvexError("Failed to create student information");
+      throw new ConvexError('Failed to create student information');
     }
     await ctx.runMutation(internal.progress.createProgress, {
       studentId: studentId,
